754b6f9ec7514b1672116053c605a3d9764b45db
xsuve <georgebaba99@yahoo.com>
2022-01-30 18:17:03 +0200

    Commit

---
+++ api.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 8000;
+const net = require('net');
+
+app.use(cors({ credentials: true, origin: true }));
+
+app.get('/', (req, res) => {
+  res.send('HotspotStatus API');
+});
+
+app.get('/ping', (req, res) => {
+  if (req.query.ip && req.query.port) {
+    const socket = new net.Socket();
+    socket.setTimeout(2500);
+    socket.on('connect', () => {
+      res.send({ status: true });
+      socket.destroy();
+    }).on('error', (e) => {
+      res.send({ status: false });
+    }).on('timeout', (e) => {
+      res.send({ status: false });
+    }).connect(req.query.port, req.query.ip);
+  } else {
+    es.send({ status: false });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}.`);
+})
